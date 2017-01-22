42b5d911cef4401b6f16ec12f3e8a3b88c45bd97
gaoziqiang <gaoziqianghi@.com>
2017-01-22 21:06:34 +0800

    PersonController

--- src/main/java/cn/edu/sdut/softlab/controller/PersonController.java
+++ src/main/java/cn/edu/sdut/softlab/controller/PersonController.java
@@ -1,7 +1,10 @@
 package cn.edu.sdut.softlab.controller;
 
+import java.io.Serializable;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import javax.management.Query;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -12,18 +15,15 @@ import javax.transaction.SystemException;
 
 import cn.edu.sdut.softlab.model.Person;
 
-
 @Named("controller")
 @RequestScoped
-public class PersonController {
-	// Message ms = new Message();
-	
+public class PersonController implements Serializable {
 
+	private static final long serialVersionUID = 1L;
 	EntityManagerFactory emf = null;
 	EntityManager em = null;
-	
+
 	private Person newPerson = new Person();
-	
 
 	// newPerson的getter和setter方法
 	public Person getNewPerson() {
@@ -34,15 +34,16 @@ public class PersonController {
 		this.newPerson = newPerson;
 	}
 
-	public void login() throws IllegalStateException, SecurityException, HeuristicMixedException, HeuristicRollbackException, RollbackException, SystemException {
+	// login方法
+	public void login() throws IllegalStateException, SecurityException, HeuristicMixedException,
+			HeuristicRollbackException, RollbackException, SystemException {
 		try {
 			emf = Persistence.createEntityManagerFactory("Person");
 			em = emf.createEntityManager();
-			
-			//断点
+			// 断点测试
 			System.out.println("打印输出newPerson:   " + newPerson.toString());
-			System.out.println("打印输出em:  " + em.toString());//测试结果，EntityManager注入失败
-			em.getTransaction().begin();//至关重要的一步：开启事务
+			System.out.println("打印输出em:  " + em.toString());// 测试结果，EntityManager注入失败
+			em.getTransaction().begin();// 至关重要的一步：开启事务
 			em.persist(newPerson);
 			em.getTransaction().commit();
 		} catch (Exception e) {
@@ -55,25 +56,38 @@ public class PersonController {
 
 	}
 
-	/*// 插入信息
-	public void insertMessage(String name, String password) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("HelloWorldJPAPU");
-		System.out.println("打印emf: " + emf.toString());
+	// 查询
+	public void query() {
 
-		Person person = new Person();
-		person.setName(name);
-		person.setPassword(password);
+		try {
+			emf = Persistence.createEntityManagerFactory("Person");
+			em = emf.createEntityManager();
+			em.getTransaction().begin();
+			System.out.println("查看em对象:" + em.toString());
 
-		EntityManager em = emf.createEntityManager();
-		System.out.println("打印em: " + em.toString());
-		em.getTransaction().begin();
+			javax.persistence.Query query = em.createQuery("select person from Person person where person.name = :name");
+			//打断点测试
+			System.out.println("查看Query对象:" + query.toString());
+			((javax.persistence.Query) query).setParameter("name", "xuqian");
+			System.out.println("查看NewQuery对象:" + query.toString());
 
-		em.persist(person);
+			Person result1 = (Person) ((javax.persistence.Query) query).getSingleResult();
+			if(result1 == null) {
+				System.out.println("当前没有该对象");
+			}
+			System.out.println("查看result对象:" + result1.toString());
 
-		em.getTransaction().commit();
+			System.out.println("打印输出Name:  " + result1.getName());
 
-		em.close();
-		emf.close();
-	}*/
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			em.close();
+			emf.close();
+			System.out.println("查询成功!");
+		}
 
+		// result.getCode();
+	}
 }
