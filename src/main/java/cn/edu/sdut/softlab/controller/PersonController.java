897440a5dd012672607404850eb74acfada5cad8
gaoziqiang <gaoziqianghi@.com>
2017-03-05 21:52:05 +0800

    session不同状态的探究

--- src/main/java/cn/edu/sdut/softlab/controller/PersonController.java
+++ src/main/java/cn/edu/sdut/softlab/controller/PersonController.java
@@ -8,6 +8,7 @@ import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceUnitUtil;
 import javax.transaction.HeuristicMixedException;
 import javax.transaction.HeuristicRollbackException;
 import javax.transaction.RollbackException;
@@ -65,50 +66,48 @@ public class PersonController implements Serializable {
 			em.getTransaction().begin();
 			System.out.println("查看em对象:" + em.toString());
 
-			javax.persistence.Query query = em.createQuery("select person from Person person where person.name = :name");
-			//打断点测试
+			javax.persistence.Query query = em
+					.createQuery("select person from Person person where person.name = :name");
+			// 打断点测试
 			System.out.println("查看Query对象:" + query.toString());
 			((javax.persistence.Query) query).setParameter("name", "gaoziqiang");
-			
-			//对query对象进行
+
+			// 对query对象进行
 			System.out.println("查看NewQuery对象:" + query.toString());
-			
-            query.getResultList();
-           
-            if(query.getResultList().size() > 0) {
-            	System.out.print("成功查询到对象");
-            	 Iterator it = query.getResultList().iterator();
-                 
-                 while(it.hasNext()) {
-                 	Person person = (Person) it.next();
-                 	System.out.println("查询结query的Name: " + person.getName());
-                 }
-            	//query.getResultList().size();
-            }else {
-            	System.out.println("该对象不存在");
-            }
-           /* query.getSingleResult().toString();
-			//强制类型转换 Object --> Person
-            Person result1 = (Person) ((javax.persistence.Query) query).getSingleResult();
-			List<Person> singleResult = (List<Person>) query.getSingleResult();
-			List<Person> personList = singleResult;
-			if(personList.size() > 0){
-				System.out.println("查看result对象:" + personList.toString());
 
-				System.out.println("打印输出Name:  " + personList.get(0));
-				
-			}else {
-				System.out.println("当前没有该对象");
-			}*/
-			/*personList.size();
-			if(result1.equals(null)) {
-				System.out.println("当前没有该对象");
-			}else {
-				System.out.println("查看result对象:" + result1.toString());
+			query.getResultList();
 
-				System.out.println("打印输出Name:  " + result1.getName());
-			}*/
-			
+			if (query.getResultList().size() > 0) {
+				System.out.print("成功查询到对象");
+				Iterator it = query.getResultList().iterator();
+
+				while (it.hasNext()) {
+					Person person = (Person) it.next();
+					System.out.println("查询结query的Name: " + person.getName());
+				}
+				// query.getResultList().size();
+			} else {
+				System.out.println("该对象不存在");
+			}
+			/*
+			 * query.getSingleResult().toString(); //强制类型转换 Object --> Person
+			 * Person result1 = (Person) ((javax.persistence.Query)
+			 * query).getSingleResult(); List<Person> singleResult =
+			 * (List<Person>) query.getSingleResult(); List<Person> personList =
+			 * singleResult; if(personList.size() > 0){
+			 * System.out.println("查看result对象:" + personList.toString());
+			 * 
+			 * System.out.println("打印输出Name:  " + personList.get(0));
+			 * 
+			 * }else { System.out.println("当前没有该对象"); }
+			 */
+			/*
+			 * personList.size(); if(result1.equals(null)) {
+			 * System.out.println("当前没有该对象"); }else {
+			 * System.out.println("查看result对象:" + result1.toString());
+			 * 
+			 * System.out.println("打印输出Name:  " + result1.getName()); }
+			 */
 
 			em.getTransaction().commit();
 		} catch (Exception e) {
@@ -121,9 +120,7 @@ public class PersonController implements Serializable {
 
 		// result.getCode();
 	}
-	
-	
-	
+
 	public void query2() {
 
 		try {
@@ -132,26 +129,46 @@ public class PersonController implements Serializable {
 			em.getTransaction().begin();
 			System.out.println("查看em对象:" + em.toString());
 
-			javax.persistence.Query query = em.createQuery("select person from Person person where person.name = :name");
-			//打断点测试
+			javax.persistence.Query query = em
+					.createQuery("select person from Person person where person.name = :name");
+			// 打断点测试
 			System.out.println("查看Query对象:" + query.toString());
-			((javax.persistence.Query) query).setParameter("name", "xuqian");
-			
-			//对query对象进行
+			((javax.persistence.Query) query).setParameter("name", "gaoziqiang111");
+			// 对query对象进行
 			System.out.println("查看NewQuery对象:" + query.toString());
-			
-            
-            query.getSingleResult().toString();
-			//强制类型转换 Object --> Person
-            Person result1 = (Person) ((javax.persistence.Query) query).getSingleResult();
-			
+
+			query.getSingleResult().toString();
+			// 强制类型转换 Object --> Person
+			Person result1 = (Person) ((javax.persistence.Query) query).getSingleResult();
+			PersistenceUnitUtil puu = emf.getPersistenceUnitUtil();
 			System.out.println("查询结果的姓名: " + result1.getName());
-			
+			System.out.println("使用PersistenceUnitUtil的getIdentifier方法检测result1的状态: " + puu.getIdentifier(result1));
+
+			em.contains(result1);
+			System.out.println("打印查看result1的我状态: " + em.contains(result1));
+			//detach一下
+			em.detach(result1);
+			//检测方法1 contains()方法
+			System.out.println("再次打印查看result1的我状态: " + em.contains(result1));
+
+			puu.getIdentifier(result1);
+			//检测方法2 getIdentifier()方法
+			System.out.println("再次使用PersistenceUnitUtil的getIdentifier方法检测result1的状态: " + puu.getIdentifier(result1));
+			// 进行remove操作
+			// em.detach(result1);
+			// em.remove(result1);
+			/*
+			 * result1.setName("gaoziqiang111");
+			 * result1.setPassword("gaoziqiang111");
+			 * System.out.println("打点测试result1对象的Name属性: " + result1.getName()
+			 * ); em.persist(result1);//可以不用merger()方法 //em.persist(result1);
+			 * System.out.println("persist成功!");
+			 */
 
 			em.getTransaction().commit();
 		} catch (Exception e) {
 			System.out.println("没有发现该对象!!!");
-			//e.printStackTrace();
+			// e.printStackTrace();
 		} finally {
 			em.close();
 			emf.close();
@@ -159,5 +176,9 @@ public class PersonController implements Serializable {
 		}
 
 		// result.getCode();
+	}
+
+	public void remove() {
+
 	}
 }
